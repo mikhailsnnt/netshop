ba19f4b37e6517ffa6c5d37b46cfbae74ce74ed6
Mikhail Nikitin <nitinin.miha@icloud.com>
2022-05-16 22:29:30 +0300

    Initial commit

---
+++ src/main/kotlin/com/sainnt/netshop/NetShopApplication.kt
@@ -0,0 +1,11 @@
+package com.sainnt.netshop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NetShopApplication
+
+fun main(args: Array<String>) {
+    runApplication<NetShopApplication>(*args)
+}
---
+++ src/test/kotlin/com/sainnt/netshop/NetShopApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sainnt.netshop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NetShopApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
