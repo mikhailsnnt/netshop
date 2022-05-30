672fbfd257eb27a2e0ec0569488aef9457eaf51d
Mikhail Nikitin <nitinin.miha@icloud.com>
2022-05-30 23:35:08 +0300

    Extracted exceptions to library

--- common-exception-lib/src/main/kotlin/com/sainnt/netshop/common/exception/NetShopApiException.kt
+++ common-exception-lib/src/main/kotlin/com/sainnt/netshop/common/exception/NetShopApiException.kt
@@ -1,4 +1,4 @@
-package com.sainnt.netshop.exception
+package com.sainnt.netshop.common.exception
 
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
