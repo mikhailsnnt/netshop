f55d00ee6ea4db966a23a54caf07a36d9707effb
Mikhail Nikitin <nitinin.miha@icloud.com>
2022-06-15 23:57:42 +0300

    Fixed JWT. Bumped version 1.2 #011

--- common-lib-jwt/src/main/kotlin/com/sainnt/netshop/jwt/utils/JwtService.kt
+++ common-lib-jwt/src/main/kotlin/com/sainnt/netshop/jwt/utils/JwtService.kt
@@ -3,15 +3,27 @@ package com.sainnt.netshop.jwt.utils
 import com.sainnt.netshop.common.exception.security.ExpiredJwtToken
 import io.jsonwebtoken.ExpiredJwtException
 import io.jsonwebtoken.Jwts
+import java.security.KeyFactory
+import java.security.PublicKey
+import java.security.spec.X509EncodedKeySpec
+import java.util.*
 
 class JwtService(private val publicKey: String) {
     fun parseClaims(token:String): JwtTokenData{
         try{
-        return Jwts.parser().setSigningKey(publicKey).parseClaimsJws(token).let {
+        return Jwts.parser().setSigningKey(getPublicKey(publicKey.toByteArray())).parseClaimsJws(token).let {
             JwtTokenData(it.body.subject, it.body.getOrDefault(ROLES_KEY, emptyList<String>()) as List<String>)
         }
         }catch (e: ExpiredJwtException){
             throw ExpiredJwtToken()
+        }
+    }
+
+    private fun getPublicKey(bytes: ByteArray): PublicKey {
+        return Base64.getDecoder().decode(bytes).let {
+            val spec = X509EncodedKeySpec(bytes)
+            val factory = KeyFactory.getInstance("RSA")
+            factory.generatePublic(spec)
         }
     }
 
