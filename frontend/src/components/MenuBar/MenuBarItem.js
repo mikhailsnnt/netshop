3d64841d795ad94b4f1eec59089b3eac44e10d3d
Mikhail Nikitin <nitinin.miha@icloud.com>
2022-07-25 01:34:45 +0300

    minor

--- frontend/src/components/MenuBar/MenuBarItem.js
+++ frontend/src/components/MenuBar/MenuBarItem.js
@@ -2,10 +2,12 @@ import styles from './MenuBarItem.module.css'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {Link} from "react-router-dom";
 
-export default  (props) => (
+const MenuBarItem = props => (
     <li>
         <Link to = {props.linkTo}>
             <FontAwesomeIcon icon={props.fontAwesomeIcon} className={`${styles['item-icon']} faa-float animated-hover`} size="2x" />
         </Link>
     </li>
 )
+
+export default MenuBarItem
